27d4a415bd1a5e83ff8d40effc07379a859fa865
Dan Weikart <danmweikart@gmail.com>
2021-03-24 09:24:43 -0700

    can save, remove notes from local storage

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ This project is from the Day 33 code along video from Udemy's 50 projects in 50 
 
 ### Overall Impression
 
-
+I made a note taking app early in my coding journey that was nearly identical to this one, so this was a good refresher on how to use local storage. I also thought it was very cool to be able to format the notes with markdown. 
 
 ### Things Learned
 
 * Can use marked CDN to parse text in markdown with ```marked()```
+* Reminder that ```localStorage.setItem(key, value)``` takes in a name/key and a value that needs to be a string, so ```JSON.stringify()```
+* To retrieve from local storage use ```localStorage.getItem(key)```, but need to parse so wrap in ```JSON.parse()```
+* Can use ```sessionStorage()``` in same way as ```localStorage()```, but data will be cleared when browser is closed
--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 const addBtn = document.getElementById("add");
 
+// retrieve notes from local storage. Need to parse to get array
+const notes = JSON.parse(localStorage.getItem("notes"));
+// if there are notes
+if (notes) {
+  // iterate through notes and call addNewNote with note as text parameter
+  notes.forEach(note => addNewNote(note));
+}
+
 // listen for click on add button to call addNewNote
 addBtn.addEventListener("click", () => addNewNote());
 
@@ -18,32 +26,46 @@ function addNewNote(text = "") {
     <div class="main ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `;
-    // get variables from note
-    const editBtn = note.querySelector(".edit");
-    const deleteBtn = note.querySelector(".delete");
-    const main = note.querySelector(".main");
-    const textArea = note.querySelector("textarea");
-    // inside textArea is text
-    textArea.value = text;
-    // inside main is text parsed in markdown
-    main.innerHTML = marked(text);
+  // get variables from note
+  const editBtn = note.querySelector(".edit");
+  const deleteBtn = note.querySelector(".delete");
+  const main = note.querySelector(".main");
+  const textArea = note.querySelector("textarea");
+  // inside textArea is text
+  textArea.value = text;
+  // inside main is text parsed in markdown
+  main.innerHTML = marked(text);
 
-    // listen for click on delete button to remove note from DOM
-    deleteBtn.addEventListener("click", () => {
-        note.remove()
-    })
-    // listen for click on edit button to toggle hidden class between main, textarea
-    editBtn.addEventListener("click", () => {
-        main.classList.toggle("hidden");
-        textArea.classList.toggle("hidden");
-    })
-    // listen for input on textArea
-    textArea.addEventListener("input", (e) => {
-        // destructure target to get value
-        const { value } = e.target
-        // inside main is value parsed in markdown
-        main.innerHTML = marked(value);
-    })
-    // append note to body (DOM)
-    document.body.appendChild(note);
+  // listen for click on delete button to remove note from DOM and updateLS
+  deleteBtn.addEventListener("click", () => {
+    note.remove();
+    updateLS();
+  });
+  // listen for click on edit button to toggle hidden class between main, textarea
+  editBtn.addEventListener("click", () => {
+    main.classList.toggle("hidden");
+    textArea.classList.toggle("hidden");
+  });
+  // listen for input on textArea
+  textArea.addEventListener("input", e => {
+    // destructure target to get value
+    const { value } = e.target;
+    // inside main is value parsed in markdown
+    main.innerHTML = marked(value);
+    // update local storage
+    updateLS();
+  });
+  // append note to body (DOM)
+  document.body.appendChild(note);
+}
+
+function updateLS() {
+  // get all textareas
+  const notesText = document.querySelectorAll("textarea");
+
+  const notes = [];
+  // iterate through all notesText, push value to notes array
+  notesText.forEach(note => notes.push(note.value));
+  // set to local storage with "notes" as key. Need to stringify notes before storing
+  localStorage.setItem("notes", JSON.stringify(notes));
 }
